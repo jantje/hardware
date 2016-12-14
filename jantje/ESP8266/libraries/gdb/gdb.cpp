94886b835ae2ef96553241d3ad0d634350afc68c
jantje <eclipse@baeyens.it>
2016-12-14 01:06:14 +0100

    trying to get debug going

---
+++ jantje/ESP8266/libraries/gdb/gdb.cpp
@@ -0,0 +1,6 @@
+#include "Arduino.h"
+#include "gdb.h"
+
+void initVariant() {
+
+}
---
+++ jantje/ESP8266/libraries/gdb/gdb.h
@@ -0,0 +1,7 @@
+#ifndef __GDB_H
+#define __GDB_H
+#include "gdbstub.h"
+
+#define RAMFUNC __attribute__ ((section( ".entry.text")))
+
+#endif
