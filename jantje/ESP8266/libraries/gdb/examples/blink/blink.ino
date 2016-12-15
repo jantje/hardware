409b89be9f9991c198fadd1b4cacce58f246eb36
jantje <eclipse@baeyens.it>
2016-12-15 21:28:09 +0100

    Added example

---
+++ jantje/ESP8266/libraries/gdb/examples/blink/blink.ino
@@ -0,0 +1,22 @@
+#include "Arduino.h"
+#include "gdb.h"
+//The setup function is called once at startup of the sketch
+void RAMFUNC setup()
+{
+#ifdef DEBUG
+	uart_div_modify(0, UART_CLK_FREQ / 115200);
+	Serial.begin(115200);
+	gdbstub_init();
+#endif
+// Add your initialization code here
+	pinMode(BUILTIN_LED, OUTPUT);
+}
+
+// The loop function is called in an endless loop
+void RAMFUNC loop()
+{
+	digitalWrite(BUILTIN_LED, HIGH);   // turn the LED on (HIGH is the voltage level)
+	delay(1000);              // wait for a second
+	digitalWrite(BUILTIN_LED, LOW);    // turn the LED off by making the voltage LOW
+	delay(1000);              // wait for a second
+}
